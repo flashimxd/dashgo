e89fac4fc3b8ca469bafcc3c5117fecad64e60f9
Rangel Netto <rangelnetto@sympla.com.br>
2021-08-20 13:43:08 -0300

    splitting into components

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,16 @@
+import { Flex, Text, Box, Avatar } from '@chakra-ui/react'
+
+export function Profile() {
+  return (
+    <Flex
+          align="center"
+    >
+      <Box mr="4" textAlign="right">
+        <Text>Rangel Netto</Text>
+        <Text color="gray.300" fontSize="small">rangel@galo.com.br</Text>
+      </Box>
+
+      <Avatar size="md" name="Rangel Netto" src="https://avatars.githubusercontent.com/u/13886651?v=4"/>
+    </Flex>  
+  )
+}
