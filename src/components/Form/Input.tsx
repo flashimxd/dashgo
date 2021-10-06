2a0ce1ae5e9a096c7aaea3f9a9bd3e1274be633b
Rangel Netto <rangelnetto@sympla.com.br>
2021-10-06 17:07:28 -0300

    starting form validation

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,13 +1,26 @@
-import { FormControl, FormLabel, Input as CInput, InputProps as CInputProps } from '@chakra-ui/react'
-
+import { forwardRef, ForwardRefRenderFunction } from 'react'
+import { 
+  FormControl,
+  FormLabel,
+  Input as CInput,
+  InputProps as CInputProps,
+  FormErrorMessage
+} from '@chakra-ui/react'
+import { FieldError } from 'react-hook-form'
 interface InputProps extends CInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ 
+  name,
+  label,
+  error = null,
+  ...rest
+  }, ref ) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
       <CInput 
         id={name} 
@@ -20,8 +33,16 @@ export function Input({ name, label, ...rest }: InputProps) {
           bgColor: 'gray.900'
         }}
         size="lg"
+        ref={ref}
         {...rest}
       />
+      {!!error && (
+        <FormErrorMessage>
+          {error.message}
+        </FormErrorMessage>
+      )}
     </FormControl>
   )
 }
+
+export const Input = forwardRef(InputBase)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/Input";
 
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+  const { formState, handleSubmit, register } = useForm()
+
+  const { errors } = formState
+  const handleSignIn: SubmitHandler<SignInFormData> = (values) => {
+    console.log({ values })
+  }
+
+  console.log({ errors })
+
   return (
     <Flex
       w="100vw"
@@ -17,10 +32,22 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail"/>
-          <Input name="password" type="password" label="Senha"/>
+          <Input 
+            name="email"
+            type="email"
+            label="E-mail"
+            error={errors.email}
+            {...register('email', { required: 'Email obrigatório' })}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            {...register('password')}
+          />
           <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
         </Stack>
       </Flex>
